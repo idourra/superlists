3e49b64ee1ee61b559f0c8ef4c3467529548f887
Pedro Urra <urrape@gmail.com>
2020-11-12 18:36:57 -0500

    page 113

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -101,7 +101,7 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox(firefox_binary="/usr/bin/firefox-esr")
         # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
